e74d7c6ac20671d6c719a1a0f0ce698107a38c2b
Carlos Huerta García <huerta2502@hotmail.com>
2020-10-07 18:55:03 -0500

    No idea

--- alphabetPower.c
+++ alphabetPower.c
@@ -7,19 +7,44 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+void displayAlphabetPower(const char * alphabet, int k){
+    int i, j;
+    char element[k];
+    for(i = 0; i < strlen(alphabet); i++){
+        printf(", ");
+        for(j = 0; j < k; j++)
+            printf("%c", alphabet[i]);
+    }
+}
 
 void alphabetPower(const char * alphabet, int k){
     int i;
-    printf("\n∑^0={€} (€ = empty string)\n");
-    for(i = 1; i < k; i++) {
-        printf("∑^%d={}\n", i);
+    if(k == 0){
+        printf("\n∑^0 = {€}\n");
+    } else {
+        if(k == 1) {
+            printf("\n∑^1 = {%c", alphabet[0]);
+            for(i = 1; i < strlen(alphabet); i++)
+                printf(", %c", alphabet[i]);
+            printf("}\n");
+        } else {
+            if (k < 20){
+                printf("\n∑^%d = {", k);
+                displayAlphabetPower(alphabet, k);
+                printf("}\n");
+            } else {
+                printf("\nInvalid int k entry.");
+            }
+        }
     }
 }
 
 void main() {
     int k;
     char alphabet[256];
-    printf("\nAlphabetPower\n@author: Carlos Huerta Garcia, Marco Antonio Ocaña\nDescription: Displays a given alphabet to the power of a given int k.\nEnter a string including the aplhabet symbols: ");
+    printf("\nAlphabetPower\n@author: Carlos Huerta Garcia, Marco Antonio Ocaña\nDescription: Displays a given alphabet to the power of a given int k.\n(€ is used as the empty string)\nEnter a string with the aplhabet symbols: ");
     fflush(stdin);
     scanf("%s", alphabet);
     printf("\nEnter an int k: ");
